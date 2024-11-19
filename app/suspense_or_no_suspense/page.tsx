de7fe3118029c466c34c1aced45a4813420710cb
chrisweb <chris963@gmail.com>
2024-11-19 17:53:53 +0100

    make it clearer which ones work and which one fails

--- app/suspense_or_no_suspense/page.tsx
+++ app/suspense_or_no_suspense/page.tsx
@@ -5,16 +5,15 @@ const Page: React.FC = () => {
         <>
             <h1>Suspense or no Suspense:</h1>
             <ul>
-                <li><Link href="/suspense_or_no_suspense/none_at_all">No React Suspense at all</Link></li>
-                <li>
-                    <Link href="/suspense_or_no_suspense/outside_canvas">Canvas parent is a React Suspense component</Link>
+                <li style={{ color: 'green' }}>[Works] <Link href="/suspense_or_no_suspense/none_at_all">No React Suspense at all</Link></li>
+                <li style={{ color: 'red' }}>[Fails] <Link href="/suspense_or_no_suspense/outside_canvas">Canvas parent is a React Suspense component</Link>
                     <ul>
                         <li>Visit this example through the link and it will work (uses next/link client navigation)</li>
                         <li>Then reload the page and you get "THREE.WebGLRenderer: Context Lost"</li>
                     </ul>
                 </li>
-                <li><Link href="/suspense_or_no_suspense/inside_canvas">Canvas first child is a React Suspense component</Link></li>
-                <li><Link href="/suspense_or_no_suspense/inside_and_outside_canvas">Canvas parent as well as first child are React Suspense components</Link></li>
+                <li style={{ color: 'green' }}>[Works] <Link href="/suspense_or_no_suspense/inside_canvas">Canvas first child is a React Suspense component</Link></li>
+                <li style={{ color: 'green' }}>[Works] <Link href="/suspense_or_no_suspense/inside_and_outside_canvas">Canvas parent as well as first child are React Suspense components</Link></li>
             </ul>
         </>
     );
